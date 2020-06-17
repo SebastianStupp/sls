7b98ca1dc0dcd146c5254a640eeef3a8a322791e
SebastianStupp <s.stupp@outlook.de>
2020-06-17 13:23:50 +0200

    Add router and header to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import React from "react";
+import { ThemeProvider } from "emotion-theming";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import theme from "./theme";
+import GlobalStyle from "./GlobalStyles";
+import Header from "./components/Header";
 
 function App() {
-  return "UnderConstruction";
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header>Test</Header>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
